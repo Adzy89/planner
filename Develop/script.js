f89ff9b2877752eee01efd3c04e97aca93bb0de4
Adzy89 <adam_pilato@hotmail.com>
2022-12-27 23:38:30 +1100

    cleaned up js

--- Develop/script.js
+++ Develop/script.js
@@ -1,40 +1,6 @@
-$( "#root" ).ready( "https://code.jquery.com/jquery-3.4.1.min.js" );
-// added root element dom and added .ready() load jqury last
-
-//var dayNow = $('#currentDay')
-//var timeNow = $('#currentTime');
-//var minateNow = $('#currentMinute');
-//var hourNow = $('#currentHour');
-
-var time12 = ['hour-9','hour-10','hour-11','hour-12','hour-13','hour-14','hour-15','hour-16','hour-17'];
-var time24 = ['13pm','14pm','15pm','16pm','17pm','18pm'];
-
-var currentDate = dayjs().format('dddd[,] DD MMMM YYYY');
-//var currentHour = dayjs().format('hh:mm:ss A');
-//var hour = dayjs().format('HH');
-
-document.getElementById("currentDay").innerHTML = currentDate;
-
-var textArea = document.getElementsByName('textArea');
-var saveBtn = document.querySelector('#saveBtn');
-
-var input = document.getElementById('#hour-10','#hour-11','#hour-12','#hour-13','#hour-14','#hour-15','#hour-16','#hour-17');
-
-
-saveBtn.addEventListener('click',function(){
-
-  localStorage.setItem("Save", JSON.stringify(input));
-
-  localStorage.getItem('Save');
-
-  console.log('Save');
-
-
-});
-
-
-
-
+  // added root element dom and added .ready() load jqury last
+var currentDate = dayjs().format('dddd, MMM DD YYYY');
+$("#currentDay").html(currentDate);
 
 
   // TODO: Add a listener for click events on the save button. This code should
@@ -43,7 +9,10 @@ saveBtn.addEventListener('click',function(){
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
+
+
+
+
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
