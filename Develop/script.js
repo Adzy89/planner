6dbaebcd14483aec0572fd8adfe48704f7372db1
Adzy89 <adam_pilato@hotmail.com>
2022-12-28 11:11:25 +1100

    created an array to store local data for time slots

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,28 @@
-  // added root element dom and added .ready() load jqury last
-var currentDate = dayjs().format('dddd, MMM DD YYYY');
-$("#currentDay").html(currentDate);
+var today= dayjs().format('dddd, MMM DD YYYY');
+$("#currentDay").html(currentDay);
 
 
+$( document ).ready(function() {
+  
+ $(".saveBtn").on('click', function () {
+   
+ // Created an object to store the description input and current time
+    var inputData = {};
+
+    inputData.descriptionInput = $(this).siblings('.description').val();
+    inputData.currentTime = $(this).parent().attr("id");
+
+    localStorage.setItem(inputData.descriptionInput,inputData.currentTime);
+ });
+
+
+});
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-
-
-
 
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
@@ -23,5 +34,5 @@ $("#currentDay").html(currentDate);
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
-  // TODO: Add code to display the current date in the header of the page.
+
 
